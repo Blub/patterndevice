328b35c2da1ea094f19e45f0000421f222095917
Wolfgang Bumiller <wolfgang.linux@bumiller.com>
2012-04-15 00:24:35 +0200

    A demonstration on how to use it in the shell

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -e /dev/pattern ] ; then
+	echo "/dev/pattern not found, please start it first"
+	echo "Use ./patterndef -n pattern"
+	echo "And make sure we have read/write access."
+	exit 1
+fi
+
+exec 7<>/dev/pattern
+
+echo It\'s working >&7
+
+cat <&7 | dd if=/dev/stdin of=/dev/stdout bs=128 count=1
+echo
+exec 7>&-
+
+echo "You should be seeing a bunch of lines above stating that it's working..."
+echo "otherwise something's wrong"
